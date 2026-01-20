9429ac9c4d1e7c9d92ca10591583e32367be3eb7
Maged Elsayed <magedelhossary663@gmail.com>
2026-01-20 13:25:47 +0400

    update vite base config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Project-Portfolio',
-  });
+  base: import.meta.env.VITE_BASE_PATH || '/Project-Portfolio-1/',
+})
+
