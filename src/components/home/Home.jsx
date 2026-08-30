f65293c0d5c67bbbd4bea4de49e4dda96ed36ce2
Maged Elsayed <magedelhossary663@gmail.com>
2026-08-30 17:41:40 +0400

    Update Home section design

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import "./HomeStyles.css";
 import { FaWhatsapp } from "react-icons/fa";
+import heroImage from "../../assets/1about-BDAUW0NV.png";
 
 function Home() {
   return (
     <section id="home" className="hero_section">
+
+      <img
+        src={heroImage}
+        alt="Web Design"
+        className="hero_bg"
+      />
+
       <div className="hero_overlay"></div>
 
-      {/* زر الواتساب ثابت */}
       <a
         href="https://wa.me/971542483423"
         className="whatsapp_float"
         target="_blank"
+        rel="noopener noreferrer"
       >
         <FaWhatsapp />
       </a>
+
     </section>
   );
 }
