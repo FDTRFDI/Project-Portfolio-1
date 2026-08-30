766f062a09df785103e5a1b4479e59fd9adc198c
Maged Elsayed <magedelhossary663@gmail.com>
2026-08-30 17:11:00 +0400

    Update Home section design

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import "./HomeStyles.css";
+import { FaWhatsapp } from "react-icons/fa";
 
 function Home() {
   return (
     <section id="home" className="hero_section">
-      <div className="hero_overlay">
+      <div className="hero_overlay"></div>
 
-        <div className="hero_content">
-          <h1>We Build Professional Websites</h1>
-          <h2>Website Development Services</h2>
-          <p>Modern, fast, secure, responsive websites for your business.</p>
-
-          <div className="hero_buttons">
-            <a href="#contact" className="btn btn-primary">Get Your Website</a>
-            <a href="#services" className="btn btn-secondary">Explore Services</a>
-          </div>
-        </div>
-
-      </div>
+      {/* زر الواتساب ثابت */}
+      <a
+        href="https://wa.me/201234567890"
+        className="whatsapp_float"
+        target="_blank"
+      >
+        <FaWhatsapp />
+      </a>
     </section>
   );
 }
